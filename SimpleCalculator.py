f5f26880f63af7d35cd077c4798292de924cc2be
Nyiko-Ngwenya <nyikongwenya@gmail.com>
2019-08-20 12:37:57 +0200

    Last answer working

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,19 +1,28 @@
 class Calculator:
-    # def __init__(self,name):
-    #     self.name = name
-        
+    lastAnswer = 0
+    # def __init__(self):
+    #     last = 0
+
     def multiply(self,*args):
         overall_num = 1
         for num in args:
             overall_num *= num
+        self.lastAnswer = overall_num
         return overall_num
 
     def addition(self,*args):
         overall_num = 0
         for num in args:
             overall_num += num
+        self.lastAnswer = overall_num
         return overall_num
+    def last(self):
+        return self.lastAnswer
 
 calculator_instance = Calculator()
 print(calculator_instance.addition(2,2,2))
+print(calculator_instance.last())
 print(calculator_instance.multiply(2,2,2))
+print(calculator_instance.last())
+
+
